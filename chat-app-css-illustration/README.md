9494e2e4a3dae8a84070a55032853e690db82f12
Volodymyr Kulyk <kulyk.vr@gmail.com>
2023-10-19 12:19:47 +0300

    edit link and animation

--- chat-app-css-illustration/README.md
+++ chat-app-css-illustration/README.md
@@ -5,10 +5,10 @@ This is a solution to the [Chat app CSS illustration challenge on Frontend Mento
 ### Links
 
 - Live Site URL: [Netlify](https://frontendmentor-solutions.netlify.app/chat-app-css-illustration/)
-- Solution URL: [Frontend Mentor](https://www.frontendmentor.io/solutions/responsive-news-homepage-olor4ieiUg)
+- Solution URL: [Frontend Mentor](https://www.frontendmentor.io/solutions/animated-chat-app-illustration-gsJJdhg5CB)
 
 ![This is an image](./design/desktop-preview.jpg)
 
 ### About component
 
-Responsive news homepage with burger menu and micro-animations onload.
+Responsive illustration with onload animation.
--- chat-app-css-illustration/script.js
+++ chat-app-css-illustration/script.js
@@ -1,34 +1,26 @@
 const bgLeft = document.getElementById("bg-item-1");
 bgLeft.style.transform = "translateY(0%)";
-//bgLeft.style.transitionDuration = "0.4s";
+bgLeft.style.transitionDuration = "0.4s";
 
 const bgRight = document.getElementById("bg-item-2");
 bgRight.style.transform = "translateY(0%)";
-//bgRight.style.transitionDuration = "0.4s";
-//bgRight.style.transitionDelay = "0.4s";
+bgRight.style.transitionDuration = "0.4s";
+bgRight.style.transitionDelay = "0.4s";
 
 const phone = document.getElementById("phone");
 phone.style.transform = "translateY(0%)";
-//phone.style.transitionDuration = "0.4s";
-//phone.style.transitionDelay = "0.8s";
+phone.style.transitionDuration = "0.4s";
+phone.style.transitionDelay = "0.8s";
 
 const text = document.getElementById("text");
 text.style.transform = "translateY(0%)";
-//text.style.transitionDuration = "0.4s";
-//text.style.transitionDelay = "1.2s";
+text.style.transitionDuration = "0.4s";
+text.style.transitionDelay = "1.2s";
 
 const input = document.getElementById("input");
 const inputBtn = document.getElementById("input-btn");
 
-/*temp*/
-const msg1 = document.getElementById("msg1");
-msg1.style.opacity = "1";
-const msg2 = document.getElementById("msg2");
-msg2.style.opacity = "1";
-/*temp*/
-
 /* First message start */
-/*
 let text1 = "Here are a few pictures. She’s a happy girl!";
 const msg1 = document.getElementById("msg1");
 
@@ -58,11 +50,9 @@ setTimeout(() => {
 setTimeout(() => {
   msg1.style.opacity = "1";
 }, 6200);
-*/
 /* First message end */
 
 /* Second message start */
-/*
 let text2 = "Can you make it?";
 const msg2 = document.getElementById("msg2");
 
@@ -92,17 +82,16 @@ setTimeout(() => {
 setTimeout(() => {
   msg2.style.opacity = "1";
 }, 8600); //8600
-*/
 /* Second message end */
 
 const msg3 = document.getElementById("msg3");
-//msg3.style.transitionDelay = "9.4s";
+msg3.style.transitionDelay = "9.4s";
 msg3.style.opacity = "1";
 
 const msg4 = document.getElementById("msg4");
-//msg4.style.transitionDelay = "10.2s";
+msg4.style.transitionDelay = "10.2s";
 msg4.style.opacity = "1";
 
 const msg5 = document.getElementById("msg5");
-//msg5.style.transitionDelay = "11s";
+msg5.style.transitionDelay = "11s";
 msg5.style.opacity = "1";
