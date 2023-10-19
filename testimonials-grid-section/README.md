7318adcb6a6522c5f5cc09e2f846cad304cad500
Volodymyr Kulyk <kulyk.vr@gmail.com>
2023-10-19 23:23:57 +0300

    testimonials-grid-section

---
+++ testimonials-grid-section/README.md
@@ -0,0 +1,14 @@
+# Frontend Mentor - Testimonials grid section solution
+
+This is a solution to the [Testimonials grid section challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/testimonials-grid-section-Nnw6J7Un7). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+### Links
+
+- Live Site URL: [Netlify](https://frontendmentor-solutions.netlify.app/testimonials-grid-section/)
+- Solution URL: [Frontend Mentor](https://www.frontendmentor.io/solutions/responsive-news-homepage-olor4ieiUg)
+
+![This is an image](./design/desktop-preview.jpg)
+
+### About component
+
+Responsive grid layout with micro-animation.
---
+++ testimonials-grid-section/script.js
@@ -0,0 +1,31 @@
+const sections = document.getElementsByClassName("section");
+const sectionsContents = document.querySelectorAll(".section > *");
+sections[0].style.trans;
+/*
+for (let i = 0; i < sections.length; i++) {
+  setTimeout(() => {
+    sections[i].style.transitionDuration = "1s";
+    sections[i].style.opacity = "1";
+    sections[i].style.transform = "scale(1)";
+  }, (i + 1) * 80);
+}
+
+setTimeout(() => {
+  for (let i = 0; i < sectionsContents.length; i++) {
+    setTimeout(() => {
+      sectionsContents[i].style.transitionDuration = "1s";
+      sectionsContents[i].style.opacity = "1";
+      sectionsContents[i].style.transform = "scale(1)";
+    }, (i + 1) * 40);
+  }
+}, 320);
+*/
+for (let i = 0; i < sections.length; i++) {
+  sections[i].style.opacity = "1";
+  sections[i].style.transform = "scale(1)";
+}
+
+for (let i = 0; i < sectionsContents.length; i++) {
+  sectionsContents[i].style.opacity = "1";
+  sectionsContents[i].style.transform = "scale(1)";
+}
